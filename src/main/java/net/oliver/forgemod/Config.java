5978c572caf10e5b7c3ab9d1e78477d84e678b07
Jin Zhou <ferryzhou@gmail.com>
2025-07-13 08:58:17 -0700

    Changes

--- src/main/java/net/oliver/forgemod/Config.java
+++ src/main/java/net/oliver/forgemod/Config.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+//Very Important Comment
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Forge's config APIs
 @Mod.EventBusSubscriber(modid = ForgeMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
