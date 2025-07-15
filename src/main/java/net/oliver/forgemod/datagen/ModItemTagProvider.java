8c633e6bf2fee9539e1d4edbf6de33afe4b8130f
Jin Zhou <ferryzhou@gmail.com>
2025-07-14 20:16:24 -0700

    Added Datagen and Deleted Files

--- src/main/java/net/oliver/forgemod/datagen/ModItemTagProvider.java
+++ src/main/java/net/oliver/forgemod/datagen/ModItemTagProvider.java
@@ -1,2 +1,31 @@
-package net.oliver.forgemod.datagen;public class ModItemTagProvider {
+package net.oliver.forgemod.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.oliver.forgemod.ForgeMod;
+import net.oliver.forgemod.item.ModItems;
+import net.oliver.forgemod.util.ModTags;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends ItemTagsProvider {
+    public ModItemTagProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> completableFuture,
+                              CompletableFuture<TagLookup<Block>> lookupCompletableFuture, @Nullable ExistingFileHelper existingFileHelper) {
+        super(packOutput, completableFuture, lookupCompletableFuture, ForgeMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        tag(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.RAW_ALEXANDRITE.get())
+                .add(Items.RAW_COPPER)
+                .add(Items.RAW_GOLD)
+                .add(Items.RAW_IRON);
+
+    }
 }
